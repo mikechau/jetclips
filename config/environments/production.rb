8b58b926443bf7429cfef1bdd73b2087156cd2c7
Mike Chau <mike@chau.io>
2015-07-25 04:36:40 -0500

    disable sprockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ JetclipApi::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = false
 
+  config.assets.enabled = false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
